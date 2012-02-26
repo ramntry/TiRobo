cb06e605b4f1efdf98ee23d3ec3248282ca96374
Roman Tereshin <ramntry@gmail.com>
2012-02-26 20:54:00 +0400

    In init.sh added choise of branche 2011.03-maintenance for openembedded

--- init.sh
+++ init.sh
@@ -7,6 +7,8 @@ echo cloning bitbake...
     git clone git://git.openembedded.org/bitbake
 echo cloning openembedded...
     git clone git://git.openembedded.org/openembedded
+    git checkout -b 2011.03-maintenance origin/2011.03-maintenance
+    
 
 if [ ! -d $BUILD_CONF_DIR ]
 then
